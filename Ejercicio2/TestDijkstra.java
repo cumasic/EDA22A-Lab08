391e99fcc2ae51757d195d8bdc67a5f4dbe4aa0f
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-08-21 20:58:39 -0500

    Se hizo una prueba en la clase TestDijkstra

--- Ejercicio2/TestDijkstra.java
+++ Ejercicio2/TestDijkstra.java
@@ -1,5 +1,16 @@
 package Ejercicio2;
-
+import java.util.Scanner;
 public class TestDijkstra {
-    
+	public static void main(String args[])
+	{
+		int nver;
+    	Scanner valores = new Scanner(System.in);
+		System.out.print("De el numero de vertices del Grafo: ");
+		nver=valores.nextInt();
+		Dijkstra grafo=new Dijkstra(nver);
+		grafo.leetecl(nver);
+		grafo.infinito(nver);
+		grafo.dijkstra(nver);
+		grafo.Imprime(nver);
+	}
 }
