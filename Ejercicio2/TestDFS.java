0839188a278e6595c647c1d9b9279d9a5a3395c3
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-08-21 20:55:47 -0500

    Se implemento una prueba en la clase Test

--- Ejercicio2/TestDFS.java
+++ Ejercicio2/TestDFS.java
@@ -1,5 +1,16 @@
 package Ejercicio2;
-
+import java.util.Scanner;
 public class TestDFS {
-    
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+          DFS G=new DFS();
+        G.Ingresar();
+        G.Mostrar();
+        System.out.println("Para buscar según DFS en este grafo");
+        System.out.println("De el vertice de partida");
+        int i=scan.nextInt();
+        System.out.println("De el vertice a llegar");
+        int j=scan.nextInt();
+        G.dfs(i,j);
+    }
 }
