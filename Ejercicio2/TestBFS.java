fb97ee833fda182b05507b0153761f455cbf2410
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-08-21 20:52:51 -0500

    Se agregó una prueba a la clase Test

--- Ejercicio2/TestBFS.java
+++ Ejercicio2/TestBFS.java
@@ -1,5 +1,12 @@
 package Ejercicio2;
-
+import java.util.Scanner;
 public class TestBFS {
-    
+	 public static void main (String[] args){
+	    	Scanner scan = new Scanner(System.in);
+		  	BFS G=new BFS();
+	    	G.Ingresar();
+	    	G.Mostrar();
+	    	System.out.println("Buscando según bfs");
+    		G.bfs(1,5);
+	}
 }
