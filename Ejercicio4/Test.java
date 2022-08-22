4a0fef21b121ef8d521666523b81159141422480
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-08-21 21:00:45 -0500

    Se hizo una prueba en la clase Test de Ejercicio4

--- Ejercicio4/Test.java
+++ Ejercicio4/Test.java
@@ -1,5 +1,16 @@
 package Ejercicio4;
-
+import java.util.Scanner;
 public class Test {
-    
+	public static void main (String[] args){
+    	Scanner scan = new Scanner(System.in);
+		Grafo G=new Grafo();
+	    G.Ingresar();
+	    G.Mostrar();
+	    System.out.println("\nIngrese un vertice para ver si esta incluido en otro");
+	    System.out.println("Vertice inicial");
+	    int ini=scan.nextInt();
+	    System.out.println("Vertice incluido");
+	    int incl=scan.nextInt();
+	    System.out.println("Es "+G.incluido(ini, incl)+" que el vertice este incluido");
+	}
 }
